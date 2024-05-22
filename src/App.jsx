03b0a853f7c7e7d8a4f6fd4ea81852c9af45aac1
cyrus_w <cyrusw89@gmail.com>
2024-05-22 18:01:28 +1200

    added health check endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import ImageUploader from './ImageUploader.jsx';
-import './App.css'
+import './App.css';
 
-import TCnav from './assets/TCnav.png'
-import TCbanner from './assets/TCbanner.png'
-import TCfooter from './assets/TCfooter.png'
-
+import TCnav from './assets/TCnav.png';
+import TCbanner from './assets/TCbanner.png';
+import TCfooter from './assets/TCfooter.png';
 
 const App = () => {
+  // Function to check the health of the backend API
+  const checkHealth = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/healthCheck`);
+      console.log(response.data); // Should log 'API is running'
+    } catch (error) {
+      console.error('Health check failed:', error);
+    }
+  };
+
+  // Use useEffect to call the health check function when the component mounts
+  useEffect(() => {
+    checkHealth();
+  }, []);
+
   return (
     <div className='body'>
-      
-      <img className='nav' src={TCnav}/>
-      <img className='banner' src={TCbanner}/>
+      <img className='nav' src={TCnav} alt="Navigation" />
+      <img className='banner' src={TCbanner} alt="Banner" />
       <ImageUploader />
-      <img className='footer' src={TCfooter}/>
+      <img className='footer' src={TCfooter} alt="Footer" />
     </div>
   );
 };
