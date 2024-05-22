db77c57ca4263fa35ea71b6ff2427a867b595e9f
cyrus_w <cyrusw89@gmail.com>
2024-05-22 14:41:05 +1200

    refresh for deploy

--- src/ImageUploader.jsx
+++ src/ImageUploader.jsx
@@ -14,35 +14,36 @@ const Upload = () => {
     };
 
     const handleSubmit = async (event) => {
-        event.preventDefault();
-        const formData = new FormData();
-        formData.append('image', file);
-
-        try {
-            const response = await axios.post('http://localhost:5001/upload', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-            setResult(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+      event.preventDefault();
+      const formData = new FormData();
+      formData.append('image', file);
+  
+      try {
+          const response = await axios.post(`${import.meta.env.VITE_API_URL}/upload`, formData, {
+              headers: {
+                  'Content-Type': 'multipart/form-data',
+              },
+          });
+          setResult(response.data);
+      } catch (error) {
+          console.error(error);
+      }
+  };
+  
 
     return (
         <div className={styles.pageBody}>
           <div className={styles.heading}>
             
             <h2>Get Car Insurance from the car experts.  It’s quick, easy and affordable!</h2>
-            <h3>First, lets find out what type of car you have using our latest AI technology,<br/> please upload an image of your car below:</h3>
+            <h3>First, lets find out what type of car you have using our latest AI technology.<br/> Please upload an image of your car below:</h3>
           </div>
           <div className={styles.uploader}>
             <form onSubmit={handleSubmit}>
                 <input type="file" onChange={handleFileChange} />
                 <button type="submit">Upload</button>
             </form>
-            {result && <div><h4>Your Car Type: {result.carType.toUpperCase()}</h4></div>}
+            <h4>Your Car Type:</h4>{result && <div><h4> {result.carType.toUpperCase()}</h4></div>}
           </div>
 
         </div>
